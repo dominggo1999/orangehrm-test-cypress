1931692d3e5dde8f59df0cba126a36c79ae98447
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-03-14 20:57:27 +0700

    chore : add test case id to auth/route-protection

--- cypress/e2e/auth/route-protection.cy.ts
+++ cypress/e2e/auth/route-protection.cy.ts
@@ -1,6 +1,7 @@
 import DashboardPage from "page-objects/DashboardPage";
 
 describe("Route Protection", () => {
+  // A_05_001
   it("Only allow dashboard access for logged-in users.", () => {
     DashboardPage.visit();
     cy.shouldIncludeLoginPageUrl();
