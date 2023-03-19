e1f57686af56166c68656c8a5c208129cc685fe9
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-03-19 12:19:54 +0700

    refactor : make the page-objects folder structure more modular

--- cypress/page-objects/index.ts
+++ cypress/page-objects/index.ts
@@ -1,6 +1,10 @@
-export { default as LoginPage } from "./LoginPage";
 export { default as DashboardPage } from "./DashboardPage";
-export { default as ResetPasswordPage } from "./ResetPasswordPage";
-export { default as UsersPage } from "./admin/UsersPage";
-export { default as AddUserPage } from "./admin/AddUserPage";
-export { default as EditUserPage } from "./admin/EditUserPage";
+
+// Auth
+export { default as LoginPage } from "./auth/LoginPage";
+export { default as ResetPasswordPage } from "./auth/ResetPasswordPage";
+
+// Admin/Users
+export { default as UsersPage } from "./admin/users/UsersPage";
+export { default as AddUserPage } from "./admin/users/AddUserPage";
+export { default as EditUserPage } from "./admin/users/EditUserPage";
